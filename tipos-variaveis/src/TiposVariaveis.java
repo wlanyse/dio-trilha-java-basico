c80a0d36aa0c9a3d68a7467316aebbeeda7bc94f
wlanyse <wlanybeckman@hotmail.com>
2022-12-31 21:08:59 -0300

    operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudem a classe String que representa textos na aplicação
+
+        String meuNome = "WLANYSE BECKMAN";
+        
+        
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR DE PI = 3.14;
+
+        
+
     }
 }
---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class operadores {
+    public static void main(String[] args) {
+
+        String nome = "WLANYSE";
+        Int idade = 49;
+        double peso = 64.4;
+        char sexo = 'F';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+    }
+}
